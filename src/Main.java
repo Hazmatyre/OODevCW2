748d7e21ac5421ccad923ee3621140f4b11e39cb
Alex <alex@bluefoxgs.co.uk>
2015-03-03 11:26:01 +0000

    Changed Dates to Strings

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Auction a1 = new Auction();
-		a1.placeBid();
+		a1.insertTestData();
+		System.out.println(a1.getStartPrice());
 	}
 
 }
--- src/auctionKernel/Auction.java
+++ src/auctionKernel/Auction.java
@@ -1,16 +1,42 @@
 package auctionKernel;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Auction implements Blockable {
 	//Figure out what data structure for bids
 	private String username;
 	private double startPrice, reservePrice;
-	private LocalDate startDate, closeDate;
+	private String startDate, closeDate; //CHANGED TO STRING ---- CHANGE BACK BEFORE SUBMITTING
 	private char status;
 	//U = under construction, P = pending
 	//0 = started, 1 = blocked, 2 = sold, 3 = not sold
 	//C = closed
+	
+	
+	//USED FOR IMPORTING TEST DATA ##IGNORE
+	Scanner s;
+	
+	public Auction(){ //Constructor
+		insertTestData();
+	}
+	
+	public void insertTestData(){
+		try {
+			s = new Scanner(new BufferedReader(new FileReader("auctions.txt")));
+			s.useDelimiter(", ");
+			username = s.next();
+			startPrice = s.nextDouble();
+			reservePrice = s.nextDouble();
+			startDate = s.next();
+			closeDate = s.next();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+	}
 	
 	public void placeBid() {
 		
